2ed78f6815e9e5490d7d468297b8162f48004985
Neil Stevenson <neil.github@secureness.co.uk>
2024-03-25 15:42:05 +0000

    Final tweaks for MINOS spring 2024 competition

--- line-follow-pid/defaults.h
+++ line-follow-pid/defaults.h
@@ -7,9 +7,9 @@ const int SLOWDOWN_SPEED_RATIO = 2; // Half speed
 const float LOOP_INTERVAL = 0.003;  // 3mS
 const float LOOP_FREQUENCY = 1/LOOP_INTERVAL;  // Hz
 #define MAX_MOTOR_VOLTS (basespeed > MAX_BASE_SPEED ? MAX_BASE_SPEED : basespeed)
-const float PID_Kp = 0.5; //0.25; //0.5;
+const float PID_Kp = 0.8; //0.5; //0.25; //0.5;
 const float PID_Ki = 0.0;
-const float PID_Kd = 0.02; //0.005; //0.02;
+const float PID_Kd = 0.02; //0.02; //0.005; //0.02;
 
 // Marker thresholds
 const int markerLowThreshold = 150;
